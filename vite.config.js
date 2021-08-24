441838c749791ec49ea1ae92205e636132537501
Harry Bayliss <harry@senses.co.uk>
2021-08-24 16:23:09 +0100

    prod build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.js"),
+            name: "vue3ganttastic",
+            fileName: (format) => `vue-3-ganttastic.${format}.js`,
+        },
+    },
+    plugins: [vue()],
+    rollupOptions: {
+        external: ["vue"],
+        output: {
+            globals: {
+                vue: "Vue",
+            },
+        },
+    },
+});
